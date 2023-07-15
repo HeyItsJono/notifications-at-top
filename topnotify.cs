6be31dd630ab4945a266370c55300c505e3a8154
HeyItsJono <freymotifs@gmail.com>
2023-07-15 17:13:09 +1000

    Merge Pull Requests with my own minor tweaks. Adds bottomleft (@vortex1942), topmiddle (@Farenheith), taskbar icon and exit option as well as github build workflow (@GabeDuarteM).

--- topnotify.cs
+++ topnotify.cs
@@ -3,6 +3,7 @@ using System.Windows.Forms;
 using System.Runtime.InteropServices;
 using System.Drawing;
 using System.Threading;
+using System.Threading.Tasks;
 
 public class Program {
 
@@ -32,74 +33,163 @@ public class Program {
     public static int Cooldown; // Since The Teams Notification Stutters, Add A Cooldown To Dismiss
 
     public static void Main(string[] args) {
+        NotifyIcon notifyIcon = new NotifyIcon();
+        notifyIcon.Icon = SystemIcons.Application;
+        notifyIcon.Visible = true;
 
-        while (true) {
+        // Create the context menu for the taskbar icon
+        ContextMenu contextMenu = new ContextMenu();
+        MenuItem exitMenuItem = new MenuItem("Exit");
+        exitMenuItem.Click += new EventHandler(ExitMenuItem_Click);
+        contextMenu.MenuItems.Add(exitMenuItem);
+        notifyIcon.ContextMenu = contextMenu;
 
-            //MS Teams Notifications
-            try{
-                var teamsHwnd = FindWindow("Chrome_WidgetWin_1", "Microsoft Teams Notification");
-                var chromeHwnd = FindWindowEx(teamsHwnd, IntPtr.Zero, "Chrome_RenderWidgetHostHWND", "Chrome Legacy Window"); // I Think MS Is Using Chrome Webview For Notifications
+        // Run the main logic in another task to avoid blocking the main one with the while loop.
+        Task.Run(() => {
+            while (true) {
 
-                //If A Notification Is Showing Up
-                if (chromeHwnd != IntPtr.Zero){
-                    Cooldown = 0;
+                //MS Teams Notifications
+                try{
+                    var teamsHwnd = FindWindow("Chrome_WidgetWin_1", "Microsoft Teams Notification");
+                    var chromeHwnd = FindWindowEx(teamsHwnd, IntPtr.Zero, "Chrome_RenderWidgetHostHWND", "Chrome Legacy Window"); // I Think MS Is Using Chrome Webview For Notifications
 
-                    if (System.AppDomain.CurrentDomain.FriendlyName == "topleft.exe"){
-                        //Sets to top left
-                        SetWindowPos(teamsHwnd, 0, 15, 15, 100, 0, SWP_NOSIZE | SWP_NOZORDER | SWP_SHOWWINDOW);
-                    }
-                    else if (System.AppDomain.CurrentDomain.FriendlyName == "topright.exe"){
-                        //Sets to top right
-                        
-                        
-                        //Get the current position of the notification window
-                        Rectangle NotifyRect = new Rectangle();
-                        GetWindowRect(teamsHwnd, ref NotifyRect);
-                        
-                        NotifyRect.Width = NotifyRect.Width - NotifyRect.X;
-                        NotifyRect.Height = NotifyRect.Height - NotifyRect.Y;
-
-                        SetWindowPos(teamsHwnd, 0, Screen.PrimaryScreen.Bounds.Width - NotifyRect.Width - 15, 15, 100, 0, SWP_NOSIZE | SWP_NOZORDER | SWP_SHOWWINDOW);
-                    }
-                }
-                else {
-                    if (Cooldown >= 30){
-                        SetWindowPos(teamsHwnd, 0, 0, -9999, -9999, 0, SWP_NOSIZE | SWP_NOZORDER | SWP_SHOWWINDOW); // Move To Off Screen
+                    //If A Notification Is Showing Up
+                    if (chromeHwnd != IntPtr.Zero){
                         Cooldown = 0;
+
+                        if (System.AppDomain.CurrentDomain.FriendlyName == "topleft.exe"){
+                            //Sets to top left
+                            SetWindowPos(teamsHwnd, 0, 15, 15, 100, 0, SWP_NOSIZE | SWP_NOZORDER | SWP_SHOWWINDOW);
+                        }
+                        else if (System.AppDomain.CurrentDomain.FriendlyName == "topright.exe"){
+                            //Sets to top right
+
+                            //Get the current position of the notification window
+                            Rectangle NotifyRect = new Rectangle();
+                            GetWindowRect(teamsHwnd, ref NotifyRect);
+
+                            NotifyRect.Width = NotifyRect.Width - NotifyRect.X;
+                            NotifyRect.Height = NotifyRect.Height - NotifyRect.Y;
+
+                            // Change the X and Y values to change the offset.
+                            NotifyRect.X = Screen.PrimaryScreen.Bounds.Width - NotifyRect.Width;
+                            NotifyRect.Y = 0;
+
+                            SetWindowPos(teamsHwnd, 0, NotifyRect.X - 15, 15, 100, 0, SWP_NOSIZE | SWP_NOZORDER | SWP_SHOWWINDOW);
+                        }
+                        else if (System.AppDomain.CurrentDomain.FriendlyName == "topmiddle.exe"){
+                            //Sets to top middle
+
+                            //Get the current position of the notification window
+                            Rectangle NotifyRect = new Rectangle();
+                            GetWindowRect(teamsHwnd, ref NotifyRect);
+
+                            NotifyRect.Width = NotifyRect.Width - NotifyRect.X;
+                            NotifyRect.Height = NotifyRect.Height - NotifyRect.Y;
+
+                            // Change the X and Y values to change the offset.
+                            NotifyRect.X = Screen.PrimaryScreen.Bounds.Width - NotifyRect.Width;
+                            NotifyRect.X /= 2;
+                            NotifyRect.Y = 0;
+
+                            SetWindowPos(teamsHwnd, 0, NotifyRect.X - 15, 15, 100, 0, SWP_NOSIZE | SWP_NOZORDER | SWP_SHOWWINDOW);
+                        }
+                        else if (System.AppDomain.CurrentDomain.FriendlyName == "bottomleft.exe"){
+                            //Sets to top middle
+
+                            //Get the current position of the notification window
+                            Rectangle NotifyRect = new Rectangle();
+                            GetWindowRect(teamsHwnd, ref NotifyRect);
+
+                            NotifyRect.Width = NotifyRect.Width - NotifyRect.X;
+                            NotifyRect.Height = NotifyRect.Height - NotifyRect.Y;
+
+                            // Change the X and Y values to change the offset.
+                            // 50PX Y Offset to match the default notification area being above taskbar
+                            NotifyRect.X = 0;
+                            NotifyRect.Y = Screen.PrimaryScreen.Bounds.Height - NotifyRect.Height - 25;
+
+                            SetWindowPos(teamsHwnd, 0, NotifyRect.X, NotifyRect.Y, 100, 0, SWP_NOSIZE | SWP_NOZORDER | SWP_SHOWWINDOW);
+                        }
                     }
-                    Cooldown += 1; // Don't Dismiss Until 30 Frames After Signal, Prevents Stutters
+                    else {
+                        if (Cooldown >= 30){
+                            SetWindowPos(teamsHwnd, 0, 0, -9999, -9999, 0, SWP_NOSIZE | SWP_NOZORDER | SWP_SHOWWINDOW); // Move To Off Screen
+                            Cooldown = 0;
+                        }
+                        Cooldown += 1; // Don't Dismiss Until 30 Frames After Signal, Prevents Stutters
+                    }
                 }
+                catch{
+                    //User Doesn't Have Teams
+                }
+
+                //Windows System Notifications
+                var hwnd = FindWindow("Windows.UI.Core.CoreWindow", "New notification");
+                if (System.AppDomain.CurrentDomain.FriendlyName == "topleft.exe"){
+                    //Sets to top left (easy peasy)
+                    //Third argument (2nd number after hwnd) is X offset, fourth argument (3rd number after hwnd) is Y offset. Negative Y offset raises position of notifcation up.
+                    SetWindowPos(hwnd, 0, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOZORDER | SWP_SHOWWINDOW);
+                }
+                else if (System.AppDomain.CurrentDomain.FriendlyName == "topright.exe"){
+                    //Sets to top right (not as easy)
+
+                    //Get the current position of the notification window
+                    Rectangle NotifyRect = new Rectangle();
+                    GetWindowRect(hwnd, ref NotifyRect);
+
+                    NotifyRect.Width = NotifyRect.Width - NotifyRect.X;
+                    NotifyRect.Height = NotifyRect.Height - NotifyRect.Y;
+
+                    // Change the X and Y values to change the offset.
+                    NotifyRect.X = Screen.PrimaryScreen.Bounds.Width - NotifyRect.Width;
+                    NotifyRect.Y = 0;
+
+                    SetWindowPos(hwnd, 0, NotifyRect.X, NotifyRect.Y, 0, 0, SWP_NOSIZE | SWP_NOZORDER | SWP_SHOWWINDOW);
+                }
+                else if (System.AppDomain.CurrentDomain.FriendlyName == "topmiddle.exe"){
+                    //Sets to top middle; same logic as top right but divide the NotifyRect.X by 2.
+
+                    //Get the current position of the notification window
+                    Rectangle NotifyRect = new Rectangle();
+                    GetWindowRect(hwnd, ref NotifyRect);
+
+                    NotifyRect.Width = NotifyRect.Width - NotifyRect.X;
+                    NotifyRect.Height = NotifyRect.Height - NotifyRect.Y;
+
+                    // Change the X and Y values to change the offset.
+                    NotifyRect.X = Screen.PrimaryScreen.Bounds.Width - NotifyRect.Width;
+                    NotifyRect.X /= 2;
+                    NotifyRect.Y = 0;
+
+                    SetWindowPos(hwnd, 0, NotifyRect.X, NotifyRect.Y, 0, 0, SWP_NOSIZE | SWP_NOZORDER | SWP_SHOWWINDOW);
+                }
+                else if (System.AppDomain.CurrentDomain.FriendlyName == "bottomleft.exe"){
+                    //Sets to bottom left
+
+                    //Get the current position of the notification window
+                    Rectangle NotifyRect = new Rectangle();
+                    GetWindowRect(hwnd, ref NotifyRect);
+
+                    NotifyRect.Width = NotifyRect.Width - NotifyRect.X;
+                    NotifyRect.Height = NotifyRect.Height - NotifyRect.Y;
+
+                    // Change the X and Y values to change the offset.
+                    // 50PX Y Offset to match the default notification area being above taskbar
+                    NotifyRect.X = 0;
+                    NotifyRect.Y = Screen.PrimaryScreen.Bounds.Height - NotifyRect.Height - 50;
+
+                    SetWindowPos(hwnd, 0, NotifyRect.X, NotifyRect.Y, 0, 0, SWP_NOSIZE | SWP_NOZORDER | SWP_SHOWWINDOW);
+                }
+
+                Thread.Sleep(10);
             }
-            catch{
-                //User Doesn't Have Teams
-            }
+        });
 
-            //Windows System Notifications
-            var hwnd = FindWindow("Windows.UI.Core.CoreWindow", "New notification");
-            if (System.AppDomain.CurrentDomain.FriendlyName == "topleft.exe"){
-                //Sets to top left (easy peasy)
-                //50PX Y offset to make the spacing even
-                SetWindowPos(hwnd, 0, 0, -50, 0, 0, SWP_NOSIZE | SWP_NOZORDER | SWP_SHOWWINDOW);
-            }
-            else if (System.AppDomain.CurrentDomain.FriendlyName == "topright.exe"){
-                //Sets to top right (not as easy)
+        Application.Run();
+    }
 
-                //Get the current position of the notification window
-                Rectangle NotifyRect = new Rectangle();
-                GetWindowRect(hwnd, ref NotifyRect);
-                
-                NotifyRect.Width = NotifyRect.Width - NotifyRect.X;
-			    NotifyRect.Height = NotifyRect.Height - NotifyRect.Y;
-
-                //50PX Y offset to make the spacing even
-                SetWindowPos(hwnd, 0, Screen.PrimaryScreen.Bounds.Width - NotifyRect.Width, -50, 0, 0, SWP_NOSIZE | SWP_NOZORDER | SWP_SHOWWINDOW);
-            }
-
-            Thread.Sleep(10);
-        }
-
-        
-        Console.ReadLine();
-
+    private static void ExitMenuItem_Click(object sender, EventArgs e) {
+        Application.Exit();
     }
 }
